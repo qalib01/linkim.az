b6410b5964dd318f6d5c84c6e232725afdc360a9
Galib <qalib.mmmdli@gmail.com>
2024-09-13 20:51:00 +0400

    Adding footer and css classes

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,21 +3,46 @@ import classes from './Header.module.css';
 
 function Header() {
     return (
-        <nav className={ classes.primary_nav + 'navbar navbar-expand-lg navbar-light bg-white fixed-to' }>
-            <div className="container px-4 px-sm-0">
-                <Link className="navbar-brand" to="/"> Logo </Link>
-                <div className="collapse navbar-collapse">
-                    <ul className="navbar-nav ms-auto">
-                        <li className="nav-item">
-                            <NavLink className={ ({isActive}) => isActive ? 'nav-link active' : 'nav-link' } to="/"> Home </NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink className={ ({isActive}) => isActive ? 'nav-link active' : 'nav-link' } to="/about"> About </NavLink>
-                        </li>
+        <nav id="primary-navbar" class="primary-nav navbar navbar-expand-lg navbar-light bg-white fixed-top font-nunito">
+        <div class="container px-4 px-sm-0">
+           <a class="navbar-brand" href="#"><img src="assets/images/logo.png" /></a>
+           <div class="collapse navbar-collapse" id="navbarNavDropdown">
+              <ul class="navbar-nav ms-auto">
+                 <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                 </li>
+                 <li class="nav-item">
+                    <a class="nav-link" href="#">About</a>
+                 </li>
+                 <li class="nav-item">
+                    <a class="nav-link" href="#">Work</a>
+                 </li>
+                 <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Services
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                       <li><a class="dropdown-item" href="#">Branding</a></li>
+                       <li><a class="dropdown-item" href="#">UI/UX</a></li>
+                       <li><a class="dropdown-item" href="#">Hybrid Apps</a></li>
+                       <li><a class="dropdown-item" href="#">Digital Marketing</a></li>
+                       <li><a class="dropdown-item" href="#">Growth Hacking</a></li>
                     </ul>
-                </div>
-            </div>
-        </nav>
+                 </li>
+                 <li class="nav-item">
+                    <a class="nav-link" href="#">Contact</a>
+                 </li>
+              </ul>
+           </div>
+           <button id="open-sidenav" class="open-sidenav bg-transparent border-0 ms-2">
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#2c333a" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-menu">
+                 <line x1="3" y1="12" x2="21" y2="12"></line>
+                 <line x1="3" y1="6" x2="21" y2="6"></line>
+                 <line x1="3" y1="18" x2="21" y2="18"></line>
+              </svg>
+           </button>
+        </div>
+     </nav>
     )
 }
 
--- src/pages/Root/PageLayout.js
+++ src/pages/Root/PageLayout.js
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Header from "../../components/Header/Header";
+import Footer from "../../components/Footer/Footer";
 
 function PageLayout() {
     return (
@@ -8,6 +9,7 @@ function PageLayout() {
             <main>
                 <Outlet />
             </main>
+            <Footer />
         </>
     )
 }
